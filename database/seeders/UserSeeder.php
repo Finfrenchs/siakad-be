4b1f130184930e6b7c15d95da54218b672f74463
M. Kelvin Madrianto F <muhammadkelvinmf@gmail.com>
2023-10-03 23:42:18 +0700

    crud user, search, pagination and user list

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,13 +14,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        User::factory(50)->create();
 
         User::create([
             'name' => 'Kelvin Admin',
             'email' => 'kelvin@fic8.com',
             'email_verified_at' => now(),
             'password' => Hash::make('123456'),
+            'roles' => 'admin',
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,15 +19,11 @@ Route::get('/', function () {
     return view('pages.auth.auth-login');
 });
 
-
-// Route::get('/', function () {
-//     return view('pages.app.dashboard-siakad', ['type_menu' => '']);
-// });
-
 Route::middleware(['auth'])->group(function () {
     Route::get('home', function(){
         return view('pages.app.dashboard-siakad', ['type_menu' => '']);
     })->name('home');
+    Route::resource('user', UserController::class);
 });
 
 // Route::get('/login', function () {
